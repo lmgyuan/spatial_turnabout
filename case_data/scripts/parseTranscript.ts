aa107e99c9a80476a17b694243f7cfba069d7b6d
Sesh Sadasivam <seshrs@umich.edu>
2024-03-04 23:25:39 -0800

    Basic testimony parsing

--- case_data/scripts/parseTranscript.ts
+++ case_data/scripts/parseTranscript.ts
@@ -111,15 +111,92 @@ function parseRawHtmlCaseTranscript(contentWrapper: Element) {
       ++childIndex;
       contextBufferLines = [];
     }
-    // TODO: This script assumes that a transcript contains ONLY MCQs
-    // (Can't handle cross-examinations yet.)
-    //
+    // CASE 2: CROSS EXAMINATION
+    else if (
+      child.tagName === "CENTER" &&
+      child.textContent.trim() === "Cross Examination"
+    ) {
+      consola.debug("Encountered cross examination");
+      const context = contextBufferLines.join("\n").trim();
+
+      const testimonies = [];
+      let testimonyLineBuffer = [];
+      ++childIndex;
+      while (childIndex < contentWrapper.children.length) {
+        const testimonyChild = contentWrapper.children[childIndex];
+        if (testimonyChild.tagName === "HR") {
+          // The testimony ends when we reach a <hr />
+          break;
+        } else if (testimonyChild.tagName === "TABLE") {
+          // Table indicates we've reached the end of one part of testimony.
+          const testimony = testimonyLineBuffer.join("\n").trim();
+          testimonyLineBuffer = [];
+
+          // First gather all possible actions
+          const actionTables = [testimonyChild];
+          ++childIndex;
+          for (; childIndex < contentWrapper.children.length; ++childIndex) {
+            if (contentWrapper.children[childIndex].tagName !== "TABLE") {
+              break;
+            }
+            actionTables.push(contentWrapper.children[childIndex]);
+          }
+          // For each action, gather metadata
+          const parsedActionTables = actionTables.map((actionTable) => {
+            const rows = Array.from(actionTable.querySelectorAll("tr"));
+            const actionRow = rows.shift();
+            const action = getTextContentFromElement(actionRow)
+              .toLowerCase()
+              .replace(/^present /, "");
+            const { isCorrect, actionText } = getActionText(rows);
+            return {
+              action,
+              isCorrect,
+              actionText,
+            };
+          });
+
+          const press = parsedActionTables.find((t) => t.action === "press");
+          const testimonyMetadata = {
+            testimony,
+            critical_press: press.isCorrect, // TODO: Check that this is correct
+            press_response: press.actionText,
+          };
+
+          const presentActions = parsedActionTables.filter(
+            (t) => t.action !== "press"
+          );
+          const correctPressAction = presentActions.filter((a) => a.isCorrect);
+          if (correctPressAction.length !== 0) {
+            // TODO: Handle alternative dialogue when presenting incorrect evidence
+            testimonyMetadata["correct_present"] = correctPressAction.map(
+              (a) => a.action
+            );
+            testimonyMetadata["present_response"] = "Objection!"; // TODO: Can we glean the correct present response?
+          }
+          testimonies.push(testimonyMetadata);
+        } else {
+          // Default case: Text is part of the testimony
+          const line = getTextContentFromElement(testimonyChild);
+          if (line !== "-- Witness's Account --") {
+            testimonyLineBuffer.push(line);
+          }
+          ++childIndex;
+        }
+      }
+      consola.debug("Finishing parsing cross examination");
+
+      caseData.push({
+        context,
+        category: "cross_examination",
+        testimonies,
+      });
+    }
     // DEFAULT CASE: Grab the text and append to the temporary "context"
     // buffer
     else {
       contextBufferLines.push(getTextContentFromElement(child));
       ++childIndex;
-      continue;
     }
   }
 
@@ -157,6 +234,34 @@ function getResponseText(el: Element): {
     responseText = responseText.replaceAll(/Leads (back )?to.*\n/gm, "");
   }
   return { isCorrect, responseText };
+}
+
+function getActionText(els: Element[]): {
+  isCorrect: boolean;
+  actionText: string;
+} {
+  const isCorrect = els.some((row) =>
+    Array.from(row.querySelectorAll("i")).some((iEl) =>
+      iEl.textContent.includes("Leads to")
+    )
+  );
+  let actionText = els
+    .map((el) => getTextContentFromElement(el))
+    .join("\n")
+    .trim();
+
+  // TODO: Find GIFs and replace with text (like "Hold it!")
+  if (isCorrect) {
+    // Remove everything after "Leads to:" since they'll be part of the next
+    // segment's context.
+    actionText = actionText.replaceAll(/Leads (back )?to[\s\S]*\n/gm, "");
+  } else {
+    // Remove the "Leads back to:", but retain the next lines since they
+    // have leading questions from the judge/prosecutor.
+    // (`.` does not match newlines.)
+    actionText = actionText.replaceAll(/Leads (back )?to.*\n/gm, "");
+  }
+  return { isCorrect, actionText };
 }
 
 main();
